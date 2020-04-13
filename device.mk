e3d9ad7996880a055a382e9311ec58a8e54e452a
MD Raza <raza231198@gmail.com>
2020-04-13 07:20:45 +0530

    X00R: import Audio Configuration from PPR1.180610.009
    Signed-off-by: MD Raza <raza231198@gmail.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,11 @@ $(call inherit-product-if-exists, vendor/asus/X00R/X00R-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(DEVICE_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
+
 # Properties
 -include $(DEVICE_PATH)/prop.mk
 
